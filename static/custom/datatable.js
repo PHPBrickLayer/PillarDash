52a6f229f205f296556f75b7f988fa16a0e7273e
Osahenrumwen Aigbogun <leonardosahon@gmail.com>
2024-07-13 10:00:03 +0100

    Updated datatable and drop zone

--- static/custom/datatable.js
+++ static/custom/datatable.js
@@ -815,169 +815,186 @@ async function hookTableOnPage({
         });
     }
 
-    function initDropzone ({api, id = "drag-drop-place", maxFileSize = 1, fileTypes, uploadFn, parallelUploads = 10, onQueueComplete}) {
-        id = "#" + id;
-        // set the dropzone container id
-        const dropzone = document.querySelector(id);
+    return loadEntries()
+}
 
-        // set the preview element template
-        const previewNode = dropzone.querySelector(".dropzone-item");
-        previewNode.id = "";
+function initDropzone ({api, id = "drag-drop-place", maxFileSize = 1, fileTypes, uploadFn, parallelUploads = 10, onQueueComplete}) {
+    id = "#" + id;
+    // set the dropzone container id
+    const dropzone = document.querySelector(id);
 
-        const previewTemplate = previewNode.parentNode.innerHTML;
-        previewNode.parentNode.removeChild(previewNode);
+    // set the preview element template
+    const previewNode = dropzone.querySelector(".dropzone-item");
+    previewNode.id = "";
 
-        const myDropzone = new Dropzone(id, { // Make the whole body a dropzone
-            url: api, // Set the url for your upload script location
-            parallelUploads: parallelUploads,
-            previewTemplate: previewTemplate,
-            maxFilesize: maxFileSize, // Max filesize in MB
-            acceptedFiles: fileTypes,
-            autoProcessQueue: false, // Stop auto upload
-            autoQueue: false, // Make sure the files aren't queued until manually added
-            previewsContainer: id + " .dropzone-items", // Define the container to display the previews
-            clickable: id + " .dropzone-select" // Define the element that should be used as click trigger to select files.
+    const previewTemplate = previewNode.parentNode.innerHTML;
+    previewNode.parentNode.removeChild(previewNode);
+
+    const myDropzone = new Dropzone(id, { // Make the whole body a dropzone
+        url: api, // Set the url for your upload script location
+        parallelUploads: parallelUploads,
+        previewTemplate: previewTemplate,
+        maxFilesize: maxFileSize, // Max filesize in MB
+        acceptedFiles: fileTypes,
+        autoProcessQueue: false, // Stop auto upload
+        autoQueue: false, // Make sure the files aren't queued until manually added
+        previewsContainer: id + " .dropzone-items", // Define the container to display the previews
+        clickable: id + " .dropzone-select" // Define the element that should be used as click trigger to select files.
+    });
+
+    myDropzone.on("addedfile", function (file) {
+        // Hook each start button
+        file.previewElement.querySelector(id + " .dropzone-start").onclick = function () {
+            const progressBar = file.previewElement.querySelector('.progress-bar');
+            progressBar.style.opacity = "1";
+            uploadFn(file, myDropzone, progressBar)
+        };
+
+        const dropzoneItems = dropzone.querySelectorAll('.dropzone-item');
+        dropzoneItems.forEach(dropzoneItem => {
+            dropzoneItem.style.display = '';
         });
+        dropzone.querySelector('.dropzone-select').classList.remove("drop-zone-custom");
+        dropzone.querySelector('.dropzone-select').classList.add("btn", "btn-sm", "btn-primary");
+        dropzone.querySelector('.dropzone-upload').style.display = "inline-block";
+        dropzone.querySelector('.dropzone-remove-all').style.display = "inline-block";
+    });
 
-        myDropzone.on("addedfile", function (file) {
-            // Hook each start button
-            file.previewElement.querySelector(id + " .dropzone-start").onclick = function () {
-                const progressBar = file.previewElement.querySelector('.progress-bar');
-                progressBar.style.opacity = "1";
-                uploadFn(file, myDropzone, progressBar)
-            };
-
-            const dropzoneItems = dropzone.querySelectorAll('.dropzone-item');
-            dropzoneItems.forEach(dropzoneItem => {
-                dropzoneItem.style.display = '';
+    // Hide the total progress bar when nothing's uploading anymore
+    myDropzone.on("complete", function (file) {
+        const progressBars = dropzone.querySelectorAll('.dz-complete');
+        setTimeout(function () {
+            progressBars.forEach(progressBar => {
+                progressBar.querySelector('.progress-bar').style.opacity = "0";
+                progressBar.querySelector('.progress').style.opacity = "0";
+                progressBar.querySelector('.dropzone-start').style.opacity = "0";
             });
-            dropzone.querySelector('.dropzone-upload').style.display = "inline-block";
-            dropzone.querySelector('.dropzone-remove-all').style.display = "inline-block";
-        });
+        }, 300);
+    });
 
-        // Hide the total progress bar when nothing's uploading anymore
-        myDropzone.on("complete", function (file) {
-            const progressBars = dropzone.querySelectorAll('.dz-complete');
-            setTimeout(function () {
-                progressBars.forEach(progressBar => {
-                    progressBar.querySelector('.progress-bar').style.opacity = "0";
-                    progressBar.querySelector('.progress').style.opacity = "0";
-                    progressBar.querySelector('.dropzone-start').style.opacity = "0";
-                });
-            }, 300);
+    // Setup the buttons for all transfers
+    dropzone.querySelector(".dropzone-upload").addEventListener('click', function () {
+        myDropzone.files.forEach(file => {
+            const progressBar = file.previewElement.querySelector('.progress-bar');
+            progressBar.style.opacity = "1";
+            uploadFn(file, myDropzone, progressBar)
         });
+    });
 
-        // Setup the buttons for all transfers
-        dropzone.querySelector(".dropzone-upload").addEventListener('click', function () {
-            myDropzone.files.forEach(file => {
-                const progressBar = file.previewElement.querySelector('.progress-bar');
-                progressBar.style.opacity = "1";
-                uploadFn(file, myDropzone, progressBar)
-            });
-        });
-
-        // Setup the button for remove all files
-        dropzone.querySelector(".dropzone-remove-all").addEventListener('click', function () {
-            Swal.fire({
-                text: "Are you sure you would like to remove all files?",
-                icon: "warning",
-                showCancelButton: true,
-                buttonsStyling: false,
-                confirmButtonText: "Yes, remove it!",
-                cancelButtonText: "No, return",
-                customClass: {
-                    confirmButton: "btn btn-primary",
-                    cancelButton: "btn btn-active-light",
-                    // lift Swal above omjs default modal
-                    container: "osai-dialogbox__appear",
-                }
-            }).then(function (result) {
-                if (result.value) {
-                    dropzone.querySelector('.dropzone-upload').style.display = "none";
-                    dropzone.querySelector('.dropzone-remove-all').style.display = "none";
-                    myDropzone.removeAllFiles(true);
-                } else if (result.dismiss === 'cancel') {
-                    Swal.fire({
-                        text: "Your files was not removed!.",
-                        icon: "error",
-                        buttonsStyling: false,
-                        confirmButtonText: "Ok, got it!",
-                        customClass: {
-                            confirmButton: "btn btn-primary",
-                            // lift Swal above omjs default modal
-                            container: "osai-dialogbox__appear",
-                        }
-                    });
-                }
-            });
-        });
-
-        // On all files completed upload
-        myDropzone.on("queuecomplete", function (progress) {
-            const uploadIcons = dropzone.querySelectorAll('.dropzone-upload');
-            uploadIcons.forEach(uploadIcon => uploadIcon.style.display = "none");
-            onQueueComplete(myDropzone, progress)
-        });
-
-        // On all files removed
-        myDropzone.on("removedfile", function (file) {
-            if (myDropzone.files.length < 1) {
+    // Setup the button for remove all files
+    dropzone.querySelector(".dropzone-remove-all").addEventListener('click', function () {
+        Swal.fire({
+            text: "Are you sure you would like to remove all files?",
+            icon: "warning",
+            showCancelButton: true,
+            buttonsStyling: false,
+            confirmButtonText: "Yes, remove it!",
+            cancelButtonText: "No, return",
+            customClass: {
+                confirmButton: "btn btn-primary",
+                cancelButton: "btn btn-active-light",
+                // lift Swal above omjs default modal
+                container: "osai-dialogbox__appear",
+            }
+        }).then(function (result) {
+            if (result.value) {
                 dropzone.querySelector('.dropzone-upload').style.display = "none";
                 dropzone.querySelector('.dropzone-remove-all').style.display = "none";
+                myDropzone.removeAllFiles(true);
+            } else if (result.dismiss === 'cancel') {
+                Swal.fire({
+                    text: "Your files was not removed!.",
+                    icon: "error",
+                    buttonsStyling: false,
+                    confirmButtonText: "Ok, got it!",
+                    customClass: {
+                        confirmButton: "btn btn-primary",
+                        // lift Swal above omjs default modal
+                        container: "osai-dialogbox__appear",
+                    }
+                });
             }
         });
-    }
+    });
 
-    function callDropFile({id = "drag-drop-place", maxFileSize = 1 }){
-        return (
-            `<div class="form-group">
-                <div class="dropzone dropzone-queue mb-2" id="${id}">
-                    <div class="dropzone-panel mb-4">
-                        <a class="dropzone-select btn btn-sm btn-primary me-2 dz-clickable">Attach files</a>
-                        <a class="dropzone-upload btn btn-sm btn-light-primary me-2">Upload All</a>
-                        <a class="dropzone-remove-all btn btn-sm btn-light-primary">Remove All</a>
-                    </div>
-            
-                    <div class="dropzone-items wm-200px">
-                        <div class="dropzone-item p-5" style="display:none">
-                            <div class="dropzone-file">
-                                <div class="dropzone-filename text-dark" title="some_image_file_name.jpg">
-                                    <span data-dz-name="">some_image_file_name.jpg</span>
-                                    <strong>(
-                                        <span data-dz-size="">340kb</span>)</strong>
-                                </div>
-                                <div class="dropzone-error mt-0" data-dz-errormessage=""></div>
+    // On all files completed upload
+    myDropzone.on("queuecomplete", function (progress) {
+        const uploadIcons = dropzone.querySelectorAll('.dropzone-upload');
+        uploadIcons.forEach(uploadIcon => uploadIcon.style.display = "none");
+        onQueueComplete(myDropzone, progress)
+    });
+
+    // On all files removed
+    myDropzone.on("removedfile", function (file) {
+        if (myDropzone.files.length < 1) {
+            dropzone.querySelector('.dropzone-select').classList.add("drop-zone-custom");
+            dropzone.querySelector('.dropzone-select').classList.remove("btn", "btn-sm", "btn-primary");
+            dropzone.querySelector('.dropzone-upload').style.display = "none";
+            dropzone.querySelector('.dropzone-remove-all').style.display = "none";
+        }
+    });
+}
+
+function callDropFile({id = "drag-drop-place", maxFileSize = 1 }){
+    return (
+        `<style>
+            .drop-zone-custom {
+                width: 100%;
+                height: 300px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                cursor: pointer;
+                background: transparent;
+                color: var(--bs-body-color);
+                font-weight: bold;
+                font-size: 2.5rem;
+                transition: ease all .8s;
+            }
+            .drop-zone-custom:hover {
+                transition: ease all .8s;
+                background: var(--bs-gray-dark);
+                color: var(--bs-light);
+            }
+        </style>
+        <div class="dropzone dropzone-queue mb-2" id="${id}">
+            <div class="dropzone-panel mb-4">
+                <a class="dropzone-select dz-button me-2 dz-clickable drop-zone-custom">Click to add files</a>
+                <a class="dropzone-upload btn btn-sm btn-light-primary me-2">Upload All</a>
+                <a class="dropzone-remove-all btn btn-sm btn-light-primary">Remove All</a>
+                
+                <div class="dropzone-items wm-200px">
+                    <div class="dropzone-item p-5" style="display:none">
+                        <div class="dropzone-file">
+                            <div class="dropzone-filename text-dark" title="some_image_file_name.jpg">
+                                <span data-dz-name="">some_image_file_name.jpg</span>
+                                <strong>(
+                                    <span data-dz-size="">340kb</span>)</strong>
                             </div>
-                            
-                            <div class="dropzone-progress">
-                                <div class="progress bg-gray-300">
-                                    <div class="progress-bar bg-primary" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0" data-dz-uploadprogress=""></div>
-                                </div>
-                            </div>
-                            
-                            <div class="dropzone-toolbar">
-                                <span class="dropzone-start">
-                                    <i class="ki-outline ki-to-right fs-1"></i>
-                                </span>
-                                <span class="dropzone-cancel" data-dz-remove="" style="display: none;">
-                                    <i class="ki-outline ki-cross fs-2"></i>
-                                </span>
-                                <span class="dropzone-delete" data-dz-remove="">
-                                    <i class="ki-outline ki-cross fs-2"></i>
-                                </span>
+                            <div class="dropzone-error mt-0" data-dz-errormessage=""></div>
+                        </div>
+                        
+                        <div class="dropzone-progress">
+                            <div class="progress bg-gray-300">
+                                <div class="progress-bar bg-primary" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0" data-dz-uploadprogress=""></div>
                             </div>
                         </div>
+                        
+                        <div class="dropzone-toolbar">
+                            <span class="dropzone-start">
+                                <i class="ki-outline ki-to-right fs-1"></i>
+                            </span>
+                            <span class="dropzone-cancel" data-dz-remove="" style="display: none;">
+                                <i class="ki-outline ki-cross fs-2"></i>
+                            </span>
+                            <span class="dropzone-delete" data-dz-remove="">
+                                <i class="ki-outline ki-cross fs-2"></i>
+                            </span>
+                        </div>
                     </div>
-            
-                    <div class="dz-default dz-message">
-                        <button class="dz-button" type="button">Drop files here to upload</button>
-                    </div>
+                    <div class="form-text text-center fs-6 text-muted mt-3">Max file size is ${maxFileSize}MB per file.</div>
                 </div>
-            
-                <span class="form-text fs-6 text-muted">Max file size is ${maxFileSize}MB per file.</span>
-            </div>`
-        )
-    }
-
-    return loadEntries()
+            </div>
+        </div>`
+    )
 }
